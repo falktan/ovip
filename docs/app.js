a2ac0dfdc1610cd8c1000645d03db9559948bd7b
Falk Tandetzky <falk.tandetzky@gmail.com>
2020-12-31 13:12:25 +0100

    Add service worker to make app offline available

--- docs/app.js
+++ docs/app.js
@@ -5,6 +5,24 @@ window.onload = async function() {
   const canvas = document.createElement("canvas");
   const backButton = document.querySelector("#back-button")
 
+  function registerServiceWorker() {
+    console.log('Registering service worker.');
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service worker registration failed, error:', error);
+      });
+    } else {
+      console.log('Service Worker feature not supported');
+    }
+  }
+
+  registerServiceWorker();
+  
   initOcr = async () => {
     const worker = Tesseract.createWorker({});
     await worker.load();
---
+++ docs/service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker install.');
+});
+
+function getCache() {
+  return caches.open('v1');
+}
+
+async function fetchAndCache(request) {
+  const response = await fetch(request);
+  cache = await getCache();
+  cache.put(request, response.clone());
+  return response;
+}
+
+async function getFromNetworkOrCache(request) {
+  try {
+    console.log('Fetching from network');
+    return fetchAndCache(request);
+  } catch {
+    console.log('Fallback to cache');
+    cache = await getCache();
+    return caches.match(request);
+  }
+}
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(getFromNetworkOrCache(event.request));
+});
